6d423d665d4270a7b44753620a7b83a899f9b861
paulsloderbeck <64672063+paulsloderbeck@users.noreply.github.com>
2020-06-09 15:39:53 -0400

    Added renderQuestion function with grade function and timer function, use qIndex to advance questions

--- script.js
+++ script.js
@@ -3,28 +3,94 @@ var answerAInput = document.querySelector("#answerAText");
 var answerBInput = document.querySelector("#answerBText");
 var answerCInput = document.querySelector("#answerCText");
 var answerDInput = document.querySelector("#answerDText");
+var timeEl = document.querySelector("#timer");
+var qIndex = 0;
+var secondsLeft = 100;
 
-const questions = ["Arrays in javascript can be used for___"];
-const answerA = ["dividends"];
-const answerB = ["assets"];
-const answerC = ["storing information"];
-const answerD = ["working out"];
+const questions = [
+    { question: "Arrays in javascript can be used for___",
+      answerA: "A: dividends", answerB: "B:assets", answerC: "C: storing an ordered list of data",
+      answerD: "D:working out", correctAnswer: "C"},
+      { question: "A function is executed when something _____ it",
+      answerA: "A: activates", answerB: "B:invokes", answerC: "C: actualizes",
+      answerD: "D:begins", correctAnswer: "B"}]
+let userScore = 0;
+let userChoice = "";
+let correctAnswer = "";
 
-renderQuestion ();
+function grade () {
+    if (userChoice === correctAnswer) {
+        userScore++;
+        alert("Correct Answer! Your current score is " + userScore);
+        qIndex++;
+        console.log(userScore);
+        renderQuestion(qIndex);
+    }
+    else  {
+        alert("Sorry, that is not the correct answer, the correct answer is " + correctAnswer);
+        qIndex++;
+        console.log(userScore);
+        renderQuestion(qIndex);
+    }
+    userChoice= "";
+}
 
-function renderQuestion() {
+function renderQuestion(i) {
     //clear previous question and answer elements
     questionInput.innerHTML= "";
     answerAInput.innerHTML= "";
     answerBInput.innerHTML= "";
     answerCInput.innerHTML= "";
     answerDInput.innerHTML= "";
+    
 // update input fields with strings from each question and answer array based on index
-for (var i = 0; i < questions.length; i++) {
-    questionInput.textContent = questions[i];
-    answerAInput.textContent = answerA[i];
-    answerBInput.textContent = answerB[i];
-    answerCInput.textContent = answerC[i];
-    answerDInput.textContent = answerD[i];
+//for (var i = 0; i < questions.length; i++) {
+    questionInput.textContent = questions[i].question;
+    answerAInput.textContent = questions[i].answerA;
+    answerBInput.textContent = questions[i].answerB;
+    answerCInput.textContent = questions[i].answerC;
+    answerDInput.textContent = questions[i].answerD;
+    correctAnswer= questions[i].correctAnswer;
+   // }   
+    
+    
+    answerAInput.addEventListener("click", function(){
+        userChoice = "A";
+        console.log(userChoice);
+        grade ();
+    });
+
+    answerBInput.addEventListener("click", function(){
+        userChoice = "B";
+        console.log(userChoice);
+        grade ();
+    });
+
+    answerCInput.addEventListener("click", function(){
+        userChoice = "C";
+        console.log(userChoice);
+        grade ();
+    });
+
+    answerDInput.addEventListener("click", function(){
+        userChoice = "D";
+        console.log(userChoice);
+        grade ();
+    });
     }
-}   
+
+function timer () {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft 
+    
+        if(secondsLeft === 0) {
+          clearInterval(timerInterval);
+          //game over;
+        }
+    
+      }, 1000);
+    }
+
+timer ();
+renderQuestion (0);
