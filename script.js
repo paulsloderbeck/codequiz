7f213dacc9ad98bfc845340e9a72feffe4ae9c2a
paulsloderbeck <64672063+paulsloderbeck@users.noreply.github.com>
2020-06-10 12:56:26 -0400

    Moved click listeners outisde of renderQuestion function to fix functionality

--- script.js
+++ script.js
@@ -1,96 +1,143 @@
-var questionInput = document.querySelector("#questionText");
-var answerAInput = document.querySelector("#answerAText");
-var answerBInput = document.querySelector("#answerBText");
-var answerCInput = document.querySelector("#answerCText");
-var answerDInput = document.querySelector("#answerDText");
-var timeEl = document.querySelector("#timer");
-var qIndex = 0;
-var secondsLeft = 100;
-
-const questions = [
-    { question: "Arrays in javascript can be used for___",
-      answerA: "A: dividends", answerB: "B:assets", answerC: "C: storing an ordered list of data",
-      answerD: "D:working out", correctAnswer: "C"},
-      { question: "A function is executed when something _____ it",
-      answerA: "A: activates", answerB: "B:invokes", answerC: "C: actualizes",
-      answerD: "D:begins", correctAnswer: "B"}]
+let questionInput = document.querySelector("#questionText");
+let answerAInput = document.querySelector("#answerAText");
+let answerBInput = document.querySelector("#answerBText");
+let answerCInput = document.querySelector("#answerCText");
+let answerDInput = document.querySelector("#answerDText");
+let timeEl = document.querySelector("#timer");
+let qIndex = 0;
+let secondsLeft = 60;
 let userScore = 0;
 let userChoice = "";
 let correctAnswer = "";
 
-function grade () {
-    if (userChoice === correctAnswer) {
-        userScore++;
-        alert("Correct Answer! Your current score is " + userScore);
-        qIndex++;
-        console.log(userScore);
-        renderQuestion(qIndex);
-    }
-    else  {
-        alert("Sorry, that is not the correct answer, the correct answer is " + correctAnswer);
-        qIndex++;
-        console.log(userScore);
-        renderQuestion(qIndex);
-    }
-    userChoice= "";
+const questions = [
+  {
+    question: "Arrays in javascript can be used for___",
+    answerA: "A: storing values across a numbered index",
+    answerB: "B: storing multiple values in a single variable",
+    answerC: "C: storing an ordered list of data",
+    answerD: "D: all of the above",
+    correctAnswer: "D",
+  },
+  {
+    question: "A function is executed when something _____ it",
+    answerA: "A: activates",
+    answerB: "B:invokes",
+    answerC: "C: actualizes",
+    answerD: "D:begins",
+    correctAnswer: "B",
+  },
+  {
+    question:
+      "The setInterval method calls a function or evaluates an expression at specified intervals in _______",
+    answerA: "A: seconds",
+    answerB: "B:nanoseconds",
+    answerC: "C: minutes",
+    answerD: "D: milliseconds",
+    correctAnswer: "D",
+  },
+  {
+    question: "The DOM stands for _____",
+    answerA: "A: Data Object Model",
+    answerB: "B: Document Object Model",
+    answerC: "C: Designated Object Model",
+    answerD: "D: Descriptive Object Model",
+    correctAnswer: "B",
+  },
+  {
+    question:
+      "Which method is used to split a string into an array of substrings?",
+    answerA: "A: repeat()",
+    answerB: "B: replace()",
+    answerC: "C: split()",
+    answerD: "D: trim()",
+    correctAnswer: "C",
+  },
+];
+
+function grade() {
+  if (userChoice === correctAnswer) {
+    userScore++;
+    alert("Correct Answer! Your current score is " + userScore);
+    qIndex++;
+    console.log(userScore);
+    renderQuestion(qIndex);
+  } else {
+    alert(
+      "Sorry, that is not the correct answer, the correct answer is " +
+        correctAnswer
+    );
+    qIndex++;
+    console.log(userScore);
+    renderQuestion(qIndex);
+  }
+  console.log("userchoice" + userChoice);
+  userChoice = "";
+  console.log("userchoice" + userChoice);
 }
 
-function renderQuestion(i) {
-    //clear previous question and answer elements
-    questionInput.innerHTML= "";
-    answerAInput.innerHTML= "";
-    answerBInput.innerHTML= "";
-    answerCInput.innerHTML= "";
-    answerDInput.innerHTML= "";
-    
-// update input fields with strings from each question and answer array based on index
-//for (var i = 0; i < questions.length; i++) {
-    questionInput.textContent = questions[i].question;
-    answerAInput.textContent = questions[i].answerA;
-    answerBInput.textContent = questions[i].answerB;
-    answerCInput.textContent = questions[i].answerC;
-    answerDInput.textContent = questions[i].answerD;
-    correctAnswer= questions[i].correctAnswer;
-   // }   
-    
-    
-    answerAInput.addEventListener("click", function(){
-        userChoice = "A";
-        console.log(userChoice);
-        grade ();
-    });
+function renderQuestion(qIndex) {
+  //clear previous question and answer elements
+  questionInput.innerHTML = "";
+  answerAInput.innerHTML = "";
+  answerBInput.innerHTML = "";
+  answerCInput.innerHTML = "";
+  answerDInput.innerHTML = "";
 
-    answerBInput.addEventListener("click", function(){
-        userChoice = "B";
-        console.log(userChoice);
-        grade ();
-    });
+  // update input fields with strings from each question and answer array based on index
+  //for (var i = 0; i < questions.length; i++) {
+  questionInput.textContent = questions[qIndex].question;
+  answerAInput.textContent = questions[qIndex].answerA;
+  answerBInput.textContent = questions[qIndex].answerB;
+  answerCInput.textContent = questions[qIndex].answerC;
+  answerDInput.textContent = questions[qIndex].answerD;
+  correctAnswer = questions[qIndex].correctAnswer;
+  // }
 
-    answerCInput.addEventListener("click", function(){
-        userChoice = "C";
-        console.log(userChoice);
-        grade ();
-    });
+  console.log("qIndex is" + qIndex);
+}
 
-    answerDInput.addEventListener("click", function(){
-        userChoice = "D";
-        console.log(userChoice);
-        grade ();
-    });
+function timer() {
+  let timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      //game over;
     }
+  }, 1000);
+}
 
-function timer () {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeEl.textContent = secondsLeft 
-    
-        if(secondsLeft === 0) {
-          clearInterval(timerInterval);
-          //game over;
-        }
-    
-      }, 1000);
-    }
+function beginQuiz() {
+  alert(
+    "Welcome to the Javascript Code Quiz. Answer the questions as quickly as possible. You have 60 seconds to complete 5 questions"
+  );
+}
+answerAInput.addEventListener("click", function () {
+  userChoice = "A";
+  console.log("userChoice" + userChoice);
+  grade();
+});
 
-timer ();
-renderQuestion (0);
+answerBInput.addEventListener("click", function () {
+  userChoice = "B";
+  console.log("userChoice" + userChoice);
+  grade();
+});
+
+answerCInput.addEventListener("click", function () {
+  userChoice = "C";
+  console.log("userChoice" + userChoice);
+  grade();
+});
+
+answerDInput.addEventListener("click", function () {
+  userChoice = "D";
+  console.log("userChoice" + userChoice);
+  grade();
+});
+
+beginQuiz();
+timer();
+renderQuestion(qIndex);
